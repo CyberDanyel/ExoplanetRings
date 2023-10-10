805020d1c5545acf70304fb82392b271400846e5
saximurus <119047390+saximurus@users.noreply.github.com>
2023-10-10 22:11:13 +0100

    Update exoring_objects.py
    added comments and also allowed for negative phase angles

--- exoring_objects.py
+++ exoring_objects.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 #planet always at origin
 #phase angle alpha is aligned with the phi of the spherical coordinate system; the star is always 'equatorial'
 
-class sphere():
+class Planet:
     def __init__(self, albedo, radius):
         '''
         Parameters
@@ -27,27 +27,67 @@ class sphere():
         The radius of the sphere.
         '''
         self.radius = radius
-        self.sc_law = lambda mu_star:albedo/np.pi # isotropic scattering law intensity distribution - 1/pi factor from normalization
-        self.phase_curve = np.vectorize(self.phase_curve_unvectorized) # this definition of phase curve includes albedo already
+        self.sc_law = lambda mu_star:albedo/np.pi # isotropic scattering law intensity distribution - 1/pi factor from normalization - currently a function to future-proof
+        self.phase_curve = np.vectorize(self.phase_curve_unvectorized) # vectorizing so that arrays of phase angles can be input more efficiently than with a Python for loop
+        # this definition of phase curve includes albedo already
         
     def get_mu_star(self, theta, phi, alpha):
-        'mu_star = cos(angle between star and normal to surface at these coords)'
-        return np.sin(theta)*(np.cos(alpha)*np.cos(phi) + np.sin(alpha)*np.sin(phi))
+        '''
+        Parameters
+        ----------
+            theta : theta in local spherical coordinate system - observer and star always at theta = pi/2
+            phi : phi in local spherical coordinate system - aligned w. phase angle
+            alpha: phase angle
+        
+        Returns
+        -------
+            mu_star = cos(angle between star and normal to surface at these coords)
+        '''
+        return np.sin(theta)*(np.cos(alpha)*np.cos(phi) + np.sin(alpha)*np.sin(phi)) # see notes for derivation
     
     def get_mu(self, theta, phi):
-        'mu = cos(angle between observer and normal to surface at these coords)'
-        return np.sin(theta)*np.cos(phi)
+        '''
+        Parameters
+        ----------
+            theta : angle theta in local spherical coordinate system - observer and star always at theta = pi/2
+            phi : agnle phi in local spherical coordinate system - aligned w. phase angle
+            alpha: phase angle
+        
+        Returns
+        -------
+            mu = cos(angle between observer and normal to surface at these coords)
+        '''
+        return np.sin(theta)*np.cos(phi) # see notes for derivation
     
     def phase_curve_integrand(self, theta, phi, alpha):
+        '''
+        Parameters
+        ----------
+            theta : angle theta in local spherical coord system - see get_mu()
+            phi : angle phi in local spherical coord system - see get_mu()
+            alpha : phase angle
+        Returns
+        -------
+        The integrand for the phase integral
+
+        '''
         mu = self.get_mu(theta, phi)
         mu_star = self.get_mu_star(theta, phi, alpha)
         return np.sin(theta) * mu * mu_star * self.sc_law(mu_star)
     
     def phase_curve_unvectorized(self, alpha):
-        return spi.dblquad(lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha), alpha-np.pi/2, np.pi/2, 0, np.pi)[0]
+        '''
+        Parameters
+        ----------
+            alpha : phase angle
+        Returns
+        -------
+        The phase curve evaluated at the phase angle alpha
+        '''
+        return spi.dblquad(lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha), max(alpha-np.pi/2, -np.pi/2), min(alpha + np.pi/2, np.pi/2), 0, np.pi)[0]
+        # the lambda allows for integration across two variables while the alpha is kept constant within the method
         
-        
-class ring():
+class Ring:
     def __init__(self, albedo, inner_rad, outer_rad, normal):
         self.inner_radius = inner_rad
         self.outer_radius = outer_rad
@@ -55,16 +95,17 @@ class ring():
         self.normal = normal
         
     def get_mu_star(self, alpha):
-        'mu_star = cos(angle between star and normal to surface)'
-        star_pos = np.array([np.cos(alpha), np.sin(alpha), 0])
+        'mu_star = cos(angle between star and normal to ring)'
+        star_pos = np.array([np.cos(alpha), np.sin(alpha), 0.])
         return np.dot(self.normal, star_pos)
     
     def get_mu(self):
-        'mu = cos(angle between observer and normal to surface'
+        'mu = cos(angle between observer and normal to ring)'
         obs_pos = np.array([1, 0, 0])
         return np.dot(self.normal, obs_pos)
         
     def phase_curve(self, alpha):
+        'phase curve innit'
         mu = self.get_mu()
         mu_star = self.get_mu_star(alpha)
         return mu * mu_star * self.sc_law(mu_star) * (mu_star > 0) # boolean prevents forwards scattering
@@ -76,22 +117,22 @@ class ring():
 
 plt.style.use('the_usual')
 
-planet = sphere(1, 1)
+planet = Planet(1, 1)
 
-ring_normal = np.array([1., 0, 0])
+ring_normal = np.array([1., 0.5, 0.2])
 ring_normal /= np.sum(ring_normal*ring_normal)
 
-r = ring(1, 2, 2.2, ring_normal)
-alphas = np.linspace(0, np.pi-0.001, 100)
+ring = Ring(1, 2, 2.2, ring_normal)
+alphas = np.linspace(-np.pi+0.001, np.pi-0.001, 100)
 
 planet_phase_curve = planet.phase_curve(alphas)
-ring_phase_curve = r.phase_curve(alphas)
+ring_phase_curve = ring.phase_curve(alphas)
 
 R = 10 # distance to star
 L = 1 # Luminosity of star
 
 planet_intensity = planet_phase_curve * (L*planet.radius**2/(4*R**2))
-ring_intensity = ring_phase_curve * (L*(r.outer_radius**2 - r.inner_radius**2)/(4*R**2))
+ring_intensity = ring_phase_curve * (L*(ring.outer_radius**2 - ring.inner_radius**2)/(4*R**2))
 
 
 plt.plot(alphas, planet_intensity, label = 'Planet')
