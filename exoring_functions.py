834862361d2aa799e0075baf9ab754c800c2908b
saximurus <119047390+saximurus@users.noreply.github.com>
2023-10-17 21:49:41 +0100

    added ring secondary eclipses
    the trick was actually normalizing the normals properly aaaaaaa

--- exoring_functions.py
+++ exoring_functions.py
@@ -9,9 +9,42 @@ import numpy as np
 # exoring_functions
 
 def integrate2d(func, bounds, n):
-    width = int(np.sqrt(n))
-    xs = np.broadcast_to(np.linspace(bounds[0][0], bounds[0][1], width), (width, width))
-    ys = np.broadcast_to(np.array([np.linspace(bounds[1][0], bounds[1][1], width)]).T, (width, width))
+    '''
+    2D integration by basic Riemann sum
+
+    Parameters
+    ----------
+    func : function object
+        The function to be integrated, with two input variables
+    bounds : iterable w. shape 2,2
+        A tuple of floats specifying the bounds of the respective variables
+    n : int
+        The number of subdivisions for each variable, total number of samples is n^2.
+
+    Returns
+    -------
+    float
+        The definite integral
+
+    '''
+    xs = np.broadcast_to(np.linspace(bounds[0][0], bounds[0][1], n), (n, n))
+    ys = np.broadcast_to(np.array([np.linspace(bounds[1][0], bounds[1][1], n)]).T, (n, n))
     area = (xs[0][1] - xs[0][0]) * (ys[1][0] - ys[0][0])
     arr = func(xs, ys)
     return np.sum(arr)*area
+
+def integrate2d_conv(func, bounds, tol, max_n):
+    width = 10
+    total_area = (bounds[0][1] - bounds[0][0]) * (bounds[1][1] - bounds[1][0])
+    old_val = func(np.mean(bounds[0]), np.mean(bounds[1])) * total_area
+    while width < max_n-2:
+        new_val = integrate2d(func, bounds, width)
+        err = new_val-old_val
+        if np.abs(err) < tol:
+            return new_val
+        else:
+            old_val = new_val
+            width=int(width*1.1)
+    print('Integration Warning:Max n reached, delta is %.3e'%err)
+    return new_val
+        
--- exoring_objects.py
+++ exoring_objects.py
@@ -13,6 +13,14 @@ import exoring_functions
 import matplotlib.pyplot as plt
 #for debugging purposes
 
+AU = 1.495978707e13
+L_SUN = 3.828e33
+R_JUP = 6.9911e9
+R_SUN = 6.957e10
+JUP_TO_AU = AU/R_JUP
+SUN_TO_JUP = R_SUN/R_JUP
+SUN_TO_AU = AU/R_SUN
+
 
 #coordinate systems defined such that the observer is always along the x-axis
 #planet always at origin
@@ -89,12 +97,16 @@ class Planet:
         -------
         The phase curve evaluated at the phase angle alpha
         '''
-        #return spi.nquad(lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha), ranges = [[0, np.pi], [max(alpha-np.pi/2, -np.pi/2), min(alpha + np.pi/2, np.pi/2)]])[0]
-        return exoring_functions.integrate2d(lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha), bounds = [[0, np.pi], [max(alpha-np.pi/2, -np.pi/2), min(alpha + np.pi/2, np.pi/2)]], n = 10000)
+        #return spi.dblquad(lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha), max(alpha-np.pi/2, -np.pi/2), min(alpha + np.pi/2, np.pi/2), 0, np.pi, epsabs = 1e-4)[0]
+        #return exoring_functions.integrate2d_conv(lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha), bounds = [[0, np.pi], [max(alpha-np.pi/2, -np.pi/2), min(alpha + np.pi/2, np.pi/2)]], tol=1e-6, max_n = 100)
+        return exoring_functions.integrate2d(lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha), bounds = [[0, np.pi], [max(alpha-np.pi/2, -np.pi/2), min(alpha + np.pi/2, np.pi/2)]], n=100)
+        
         # the lambda allows for integration across two variables while the alpha is kept constant within the method
         
     def secondary_eclipse(self, theta, phi, alpha):
         'returns boolean of whether these coords are eclipsed at this phase angle'
+        if np.abs(alpha) > 2.1*self.star.radius/self.star.distance:
+            return 1.
         return ((self.radius*np.sin(theta)*np.sin(phi) - self.star.distance*np.sin(alpha))**2 + (self.radius*np.cos(theta))**2 > self.star.radius**2)
         
     def light_curve(self, alpha):
@@ -108,6 +120,7 @@ class Ring:
         self.outer_radius = outer_rad
         self.sc_law = lambda mu_star:albedo/np.pi # isotropic scattering law intensity distribution - 1/pi factor from normalization
         self.normal = normal
+        self.secondary_eclipse = np.vectorize(self.unvectorized_secondary_eclipse)
         self.star = star
         
     def get_mu_star(self, alpha):
@@ -124,15 +137,44 @@ class Ring:
         'phase curve innit'
         mu = self.get_mu()
         mu_star = self.get_mu_star(alpha)
-        return mu * mu_star * self.sc_law(mu_star) * (mu_star > 0) # boolean prevents forwards scattering
+        return mu * mu_star * self.sc_law(mu_star) * (mu_star > 0) * self.secondary_eclipse(alpha) # boolean prevents forwards scattering
     
-    def secondary_eclipse(self, alpha):
-        'finds the amount to subtract from the ring - since there is no integral'
-        #struggling
+    def unvectorized_secondary_eclipse(self, alpha):
+        'finds the amount of flux to subtract from the ring - since there is no integral for the total ring scattering'
+        
+        if np.abs(alpha) > 2.1*self.star.radius/self.star.distance:
+            return 1.
+
+        mu = self.get_mu()
+        n_x, n_y, n_z = self.normal
+        
+        
+        y_star = self.star.distance * np.sin(alpha)
+        z_star = 0.
+        
+        #bounds_z = [max(-self.outer_radius, -self.star.radius), min(self.outer_radius, self.star.radius)]
+        #bounds_y = [max(-self.outer_radius, y_star - self.star.radius), min(self.outer_radius, y_star + self.star.radius)]
+        bounds_z = [-self.outer_radius, self.outer_radius]
+        bounds_y = [-self.outer_radius, self.outer_radius]
+        sin_theta = np.sqrt(1-mu**2)
+        cos_phi = n_z/sin_theta
+        sin_phi = n_y/sin_theta
+        
+        ring_distance = lambda y, z: np.sqrt((y*cos_phi+z*sin_phi)**2 + (1/mu**2)*(-y*sin_phi+z*cos_phi)**2)
+        
+        on_ring = lambda y, z: ring_distance(y, z) > self.inner_radius
+        in_ring = lambda y, z: ring_distance(y, z) < self.outer_radius
+        
+        in_shadow = lambda y, z: (y-y_star)**2 + (z-z_star)**2 < self.star.radius**2
+
+        numerator = exoring_functions.integrate2d(lambda y, z:on_ring(y, z)*in_ring(y, z)*in_shadow(y, z), [bounds_y, bounds_z], n=100)
+        denominator = exoring_functions.integrate2d(lambda y, z:on_ring(y, z)*in_ring(y, z), [bounds_y, bounds_z], n=100)# - self.inner_radius**2)#*mu_star
+
+        return 1 - numerator/denominator
     
     def light_curve(self, alpha):
         return (self.outer_radius**2-self.inner_radius**2)*self.phase_curve(alpha)*self.star.luminosity/(4*self.star.distance**2)
-        
+    
 class Star:
     def __init__(self, luminosity, radius, distance, mass):
         self.luminosity = luminosity
@@ -144,28 +186,35 @@ class Star:
 
 #debugging stuff
 
+
 plt.style.use('the_usual')
 
-star = Star(1000000, 10, 100, 10)
+star = Star(1, 1*SUN_TO_JUP, .1*JUP_TO_AU, 1)
 
-planet = Planet(1, 1, star)
+planet = Planet(0.52, 1, star)
 
-ring_normal = np.array([1., 1., 0.0])
-ring_normal /= np.sum(ring_normal*ring_normal)
+ring_normal = np.array([1., 1., 0.])
+ring_normal /= np.sqrt(np.sum(ring_normal*ring_normal))
 
-ring = Ring(1, 2, 2.2, ring_normal, star)
-alphas = np.linspace(-np.pi, np.pi, 1000)
+ring_normal2 = np.array([1., 0., 0.0])
+ring_normal2 /= np.sqrt(np.sum(ring_normal*ring_normal))
+
+ring = Ring(0.7, 1, 2., ring_normal, star)
+#ring2 = Ring(0.8, 1, 10, ring_normal2, star)
+
+alphas = np.array(list(np.linspace(-np.pi, -0.1, 1000)) + list(np.linspace(-0.1, 0.1, 2000)) + list(np.linspace(0.1, np.pi, 1000)))
 
 
 planet_curve = planet.light_curve(alphas)
 ring_curve = ring.light_curve(alphas)
-
+#ring2_curve = ring2.light_curve(alphas)
 plt.plot(alphas, planet_curve, label = 'Planet')
 plt.plot(alphas, ring_curve, label = 'Ring')
-plt.plot(alphas, planet_curve + ring_curve, label = 'Ring + Planet')
+plt.plot(alphas, planet_curve + ring_curve, label = 'Planet + Ring')
+
 
 plt.xlabel(r'Phase angle $\alpha$')
-plt.ylabel(r'Intensity (arbitrary)')
+plt.ylabel(r'Intensity ($L_{\odot}$)')
 plt.legend()
 
 plt.tight_layout()
