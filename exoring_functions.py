37514e8050b52615f22ee0146e2a024783bf73ad
CyberDanyel <cyberdan100@gmail.com>
2023-10-17 18:53:12 +0100

    Cleaned code

--- exoring_functions.py
+++ exoring_functions.py
@@ -6,7 +6,6 @@ Created on Fri Oct 13 17:46:48 2023
 """
 import time
 from fractions import Fraction
-from random import random
 import matplotlib.animation as animation
 import numpy as np
 from matplotlib import pyplot as plt
@@ -14,9 +13,10 @@ from matplotlib.ticker import FuncFormatter
 
 import matplotlib.ticker as tck
 
+
 # exoring_functions
 
-def integrate2d(func, bounds, sigma):
+def integrate2d(func, bounds, sigma=0.01):
     old_total_integral = 0
     iteration = 1
     n = 1000
@@ -34,48 +34,56 @@ def integrate2d(func, bounds, sigma):
             old_total_integral = new_total_integral
             pass
         else:
-            if old_total_integral == 0 or abs((new_total_integral - old_total_integral) / old_total_integral) < 0.01 or old_total_integral < 1e-10:
+            if old_total_integral == 0 or abs((
+                                                      new_total_integral - old_total_integral) / old_total_integral) < sigma or old_total_integral < 1e-10:
                 return new_total_integral
             else:
-                n = n*2
+                n = n * 2
                 width = int(np.sqrt(n))
                 iteration += 1
                 old_total_integral = new_total_integral
                 pass
+
+
 class Integrals:
-    def __init__(self,i):
+    def __init__(self, i):
         self.i = i
-        xs = np.random.uniform(0,1,self.i)
+        xs = np.random.uniform(0, 1, self.i)
         self.thetas = np.arccos(1 - 2 * xs)
-        #self.phis = np.random.uniform(- np.pi / 2, np.pi / 2,self.i)
-    def monte_carlo_integration(self,alpha,func):
-        q_sum = 0
-        n = 0
+        # self.phis = np.random.uniform(- np.pi / 2, np.pi / 2,self.i)
+
+    def monte_carlo_integration(self, alpha, func):
         if alpha >= 0:
-            #phis = [phi for phi in self.phis if alpha - np.pi / 2 < phi < np.pi / 2]
-            phis = np.random.uniform(alpha - np.pi / 2, np.pi / 2,self.i)
+            # phis = [phi for phi in self.phis if alpha - np.pi / 2 < phi < np.pi / 2]
+            phis = np.random.uniform(alpha - np.pi / 2, np.pi / 2, self.i)
         else:
-            #phis = [phi for phi in self.phis if -np.pi / 2 < phi < alpha + np.pi / 2]
-            phis = np.random.uniform(-np.pi / 2, alpha + np.pi / 2,self.i)
+            # phis = [phi for phi in self.phis if -np.pi / 2 < phi < alpha + np.pi / 2]
+            phis = np.random.uniform(-np.pi / 2, alpha + np.pi / 2, self.i)
         if alpha >= 0:
             qs = func(self.thetas, phis) * (2 * (np.pi - alpha)) / np.sin(self.thetas)
         else:
             qs = func(self.thetas, phis) * (2 * (np.pi + alpha)) / np.sin(self.thetas)
         q_average = sum(qs) / self.i
-        sigma_squared = (1/(self.i-1))*sum((qs-q_average)**2)
-        sigma = np.sqrt(sigma_squared)
-        error = (1/np.sqrt(self.i))*sigma
-        fractional_error = error/q_average
-        #print(f'q_average = {q_average} +-', f'{fractional_error*100}%')
+        # sigma_squared = (1 / (self.i - 1)) * sum((qs - q_average) ** 2)
+        # sigma = np.sqrt(sigma_squared)
+        # error = (1 / np.sqrt(self.i)) * sigma
+        # fractional_error = error / q_average
+        # print(f'q_average = {q_average} +-', f'{fractional_error*100}%')
         return q_average
+
+
 class Animation:
-    def __init__(self,planet,star,ring):
+    def __init__(self, planet, star, ring):
         self.star = star
         self.planet = planet
         self.ring = ring
+        self.alphas = np.linspace(-np.pi, np.pi, 1000)
+        self.planet_curve = None
+        self.ring_curve = None
+        self.maximum_intensity = None
         self.calculate_light_curves()
 
-    def set_axes_equal(self,ax):
+    def set_axes_equal(self, ax):
         """
         Make axes of 3D plot have equal scale so that spheres appear as spheres,
         cubes as cubes, etc.
@@ -103,20 +111,21 @@ class Animation:
         ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
         ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
 
-    def format_fraction_with_pi(self,x, pos):
+    def format_fraction_with_pi(self, x, pos):
         fract = Fraction(x).limit_denominator()
         if fract == 0:
             return "0"
         elif x == 1:
-            return '$\pi$'
+            return '$\\pi$'
         elif x == -1:
-            return '$-\pi$'
+            return '$-\\pi$'
         else:
             if fract.numerator > 0:
-                return f'$\\frac{{{fract.numerator}}}{{{fract.denominator}}}$' + '$\pi$'
+                return f'$\\frac{{{fract.numerator}}}{{{fract.denominator}}}$' + '$\\pi$'
             else:
-                return f'$-\\frac{{{abs(fract.numerator)}}}{{{fract.denominator}}}$' + '$\pi$'
-    def generate_sphere_coords(self,centre, sphere_radius, sampling_num):
+                return f'$-\\frac{{{abs(fract.numerator)}}}{{{fract.denominator}}}$' + '$\\pi$'
+
+    def generate_sphere_coords(self, centre, sphere_radius, sampling_num):
         theta = np.radians(np.linspace(0, 180, sampling_num, endpoint=True))
         phi = np.radians(np.linspace(0, 360, sampling_num, endpoint=True))
         theta_coords, phi_coords = np.meshgrid(theta, phi)
@@ -124,32 +133,37 @@ class Animation:
         y_coords = sphere_radius * np.sin(theta_coords) * np.sin(phi_coords) + centre[1]  # Offset by centre of sphere
         z_coords = sphere_radius * np.cos(theta_coords) + centre[2]  # Offset by centre of sphere
         return x_coords, y_coords, z_coords
+
     def calculate_light_curves(self):
         # fig, ax = plt.subplots()
-        self.alphas = np.linspace(-np.pi, np.pi, 1000)
         print('started')
         a = time.time()
         self.planet_curve = self.planet.light_curve(self.alphas)
         self.ring_curve = self.ring.light_curve(self.alphas)
         b = time.time()
-        self.maximum_intensity = max(self.planet_curve+self.ring_curve)
+        self.maximum_intensity = max(self.planet_curve + self.ring_curve)
         print(f'ended in ' + str(b - a))
+
     def generate_animation(self):
         plt.style.use('the_usual.mplstyle')
         fig = plt.figure(figsize=plt.figaspect(2.))
         axs1 = fig.add_subplot(3, 1, 1)
         axs2 = fig.add_subplot(3, 1, (2, 3), projection='3d')
         fig.tight_layout()
+
         def init():
             axs1.xaxis.set_major_locator(tck.MultipleLocator(base=1 / 2))
             axs1.xaxis.set_major_formatter(FuncFormatter(self.format_fraction_with_pi))
-            axs1.set_ylim(0, 1.1*self.maximum_intensity)
+            axs1.set_ylim(0, 1.1 * self.maximum_intensity)
             axs1.set_xlim(-1, 1)
             axs1.set_xlabel(r'Phase angle $\alpha$')
             axs1.set_ylabel(r'Intensity (arbitrary)')
-            axs2.set_xlim(-(self.star.distance + self.star.radius + self.planet.radius), self.star.distance + self.star.radius + self.planet.radius)
-            axs2.set_ylim(-(self.star.distance + self.star.radius + self.planet.radius), self.star.distance + self.star.radius + self.planet.radius)
-            axs2.set_zlim(-(self.star.distance + self.star.radius + self.planet.radius), self.star.distance + self.star.radius + self.planet.radius)
+            axs2.set_xlim(-(self.star.distance + self.star.radius + self.planet.radius),
+                          self.star.distance + self.star.radius + self.planet.radius)
+            axs2.set_ylim(-(self.star.distance + self.star.radius + self.planet.radius),
+                          self.star.distance + self.star.radius + self.planet.radius)
+            axs2.set_zlim(-(self.star.distance + self.star.radius + self.planet.radius),
+                          self.star.distance + self.star.radius + self.planet.radius)
             axs2.set_box_aspect([10, 10, 10])
             axs2.view_init(elev=0, azim=0)
             self.set_axes_equal(axs2)
@@ -162,9 +176,12 @@ class Animation:
             fig.tight_layout()
             print('Frame', str(frame))
             axs2.clear()
-            axs2.set_xlim(-(self.star.distance + self.star.radius + self.planet.radius), self.star.distance + self.star.radius + self.planet.radius)
-            axs2.set_ylim(-(self.star.distance + self.star.radius + self.planet.radius), self.star.distance + self.star.radius + self.planet.radius)
-            axs2.set_zlim(-(self.star.distance + self.star.radius + self.planet.radius), self.star.distance + self.star.radius + self.planet.radius)
+            axs2.set_xlim(-(self.star.distance + self.star.radius + self.planet.radius),
+                          self.star.distance + self.star.radius + self.planet.radius)
+            axs2.set_ylim(-(self.star.distance + self.star.radius + self.planet.radius),
+                          self.star.distance + self.star.radius + self.planet.radius)
+            axs2.set_zlim(-(self.star.distance + self.star.radius + self.planet.radius),
+                          self.star.distance + self.star.radius + self.planet.radius)
             axs2.set_box_aspect([10, 10, 10])
             axs2.view_init(elev=0, azim=0)
             self.set_axes_equal(axs2)
@@ -177,13 +194,15 @@ class Animation:
             axs1.xaxis.set_major_formatter(FuncFormatter(self.format_fraction_with_pi))
             axs1.xaxis.set_major_locator(tck.MultipleLocator(base=1 / 2))
             axs1.set_xlim(-1, 1)
-            axs1.set_ylim(0, 1.1*self.maximum_intensity)
+            axs1.set_ylim(0, 1.1 * self.maximum_intensity)
             centre = [self.star.distance * np.cos(alpha - np.pi), self.star.distance * np.sin(alpha - np.pi),
                       z]  # np.pi factor corrects for the beginning of the phase
-            star_x_coords, star_y_coords, star_z_coords = self.generate_sphere_coords([0, 0, 0], sphere_radius=self.star.radius,
-                                                                                 sampling_num=100)
-            x_coords, y_coords, z_coords = self.generate_sphere_coords(centre + orbital_centre, sphere_radius=self.planet.radius,
-                                                                  sampling_num=100)
+            star_x_coords, star_y_coords, star_z_coords = self.generate_sphere_coords([0, 0, 0],
+                                                                                      sphere_radius=self.star.radius,
+                                                                                      sampling_num=100)
+            x_coords, y_coords, z_coords = self.generate_sphere_coords(centre + orbital_centre,
+                                                                       sphere_radius=self.planet.radius,
+                                                                       sampling_num=100)
             axs2.plot_surface(
                 star_x_coords, star_y_coords, star_z_coords, color='orange',
                 linewidth=0, antialiased=False, rstride=1, cstride=1, alpha=1)
--- exoring_objects.py
+++ exoring_objects.py
@@ -4,73 +4,71 @@ Created on Sun Oct  8 17:51:16 2023
 
 @author: victo
 """
-import time
-
 # exoring objects
 
 import numpy as np
-import scipy.integrate as spi
 import exoring_functions
 import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter, FormatStrFormatter
-from fractions import Fraction
-import matplotlib.ticker as tck
-#for debugging purposes
 
 
-#coordinate systems defined such that the observer is always along the x-axis
-#planet always at origin
-#phase angle alpha is aligned with the phi of the spherical coordinate system; the star is always 'equatorial'
-
-#everything also assumes circular orbits
+# for debugging purposes
 
 
-    
+# coordinate systems defined such that the observer is always along the x-axis
+# planet always at origin
+# phase angle alpha is aligned with the phi of the spherical coordinate system; the star is always 'equatorial'
+
+# everything also assumes circular orbits
+
+
 class Planet:
     def __init__(self, albedo, radius, star):
-        '''
+        """
         Parameters
         ----------
             radius : float
         The radius of the sphere.
-        '''
+        """
         self.radius = radius
-        self.sc_law = lambda mu_star:albedo/np.pi # isotropic scattering law intensity distribution - 1/pi factor from normalization - currently a function to future-proof
-        self.phase_curve = np.vectorize(self.phase_curve_unvectorized) # vectorizing so that arrays of phase angles can be input more efficiently than with a Python for loop
+        self.sc_law = lambda \
+            mu_star: albedo / np.pi  # isotropic scattering law intensity distribution - 1/pi factor from
+        # normalization - currently a function to future-proof
+        self.phase_curve = np.vectorize(
+            self.phase_curve_unvectorized)  # vectorizing so that arrays of phase angles can be input more
+        # efficiently than with a Python for loop
         self.star = star
         self.Integrals = exoring_functions.Integrals(100000)
         # this definition of phase curve includes albedo already
-        
+
     def get_mu_star(self, theta, phi, alpha):
-        '''
+        """
         Parameters
         ----------
             theta : theta in local spherical coordinate system - observer and star always at theta = pi/2
             phi : phi in local spherical coordinate system - aligned w. phase angle
             alpha: phase angle
-        
+
         Returns
         -------
             mu_star = cos(angle between star and normal to surface at these coords)
-        '''
-        return np.sin(theta)*(np.cos(alpha)*np.cos(phi) + np.sin(alpha)*np.sin(phi)) # see notes for derivation
-    
+        """
+        return np.sin(theta) * (np.cos(alpha) * np.cos(phi) + np.sin(alpha) * np.sin(phi))  # see notes for derivation
+
     def get_mu(self, theta, phi):
-        '''
+        """
         Parameters
         ----------
             theta : angle theta in local spherical coordinate system - observer and star always at theta = pi/2
-            phi : agnle phi in local spherical coordinate system - aligned w. phase angle
-            alpha: phase angle
-        
+            phi : angle phi in local spherical coordinate system - aligned w. phase angle
+
         Returns
         -------
             mu = cos(angle between observer and normal to surface at these coords)
-        '''
-        return np.sin(theta)*np.cos(phi) # see notes for derivation
-    
+        """
+        return np.sin(theta) * np.cos(phi)  # see notes for derivation
+
     def phase_curve_integrand(self, theta, phi, alpha):
-        '''
+        """
         Parameters
         ----------
             theta : angle theta in local spherical coord system - see get_mu()
@@ -80,64 +78,75 @@ class Planet:
         -------
         The integrand for the phase integral
 
-        '''
+        """
         mu = self.get_mu(theta, phi)
         mu_star = self.get_mu_star(theta, phi, alpha)
-        return np.sin(theta) * mu * mu_star * self.sc_law(mu_star) * self.secondary_eclipse(theta, phi, alpha)# * (np.abs(alpha) >= np.arcsin((self.star.radius + self.radius) / self.star.distance))
-        #Only check secondary eclipse for certain alphas when you are close to the star for speed
-    def phase_curve_unvectorized(self, alpha: object) -> object:
-        '''
+        return np.sin(theta) * mu * mu_star * self.sc_law(mu_star) * self.secondary_eclipse(theta, phi,
+                                                                                            alpha)  # * (np.abs(
+        # alpha) >= np.arcsin((self.star.radius + self.radius) / self.star.distance))
+        # Only check secondary eclipse for certain alphas when you are close to the star for speed
+
+    def phase_curve_unvectorized(self, alpha: float) -> object:
+        """
         Parameters
         ----------
             alpha : phase angle
         Returns
         -------
         The phase curve evaluated at the phase angle alpha
-        '''
-        #return spi.nquad(lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha), ranges = [[0, np.pi], [max(alpha-np.pi/2, -np.pi/2), min(alpha + np.pi/2, np.pi/2)]])[0]
-        return exoring_functions.integrate2d(lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha), bounds = [[0, np.pi], [max(alpha-np.pi/2, -np.pi/2), min(alpha + np.pi/2, np.pi/2)]], sigma = 1e-3)
-        #return self.Integrals.monte_carlo_integration(alpha,lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha)) # Monte Carlo
-        # the lambda allows for integration across two variables while the alpha is kept constant within the method
-        
+        """
+        # return spi.nquad(lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha), ranges = [[0, np.pi],
+        # [max(alpha-np.pi/2, -np.pi/2), min(alpha + np.pi/2, np.pi/2)]])[0]
+        return exoring_functions.integrate2d(lambda theta, phi: self.phase_curve_integrand(theta, phi, alpha),
+                                             bounds=[[0, np.pi], [max(alpha - np.pi / 2, -np.pi / 2),
+                                                                  min(alpha + np.pi / 2, np.pi / 2)]], sigma=1e-3)
+        # return self.Integrals.monte_carlo_integration(alpha,lambda theta, phi: self.phase_curve_integrand(theta,
+        # phi, alpha)) # Monte Carlo the lambda allows for integration across two variables while the alpha is kept
+        # constant within the method
+
     def secondary_eclipse(self, theta, phi, alpha):
-        'returns False if these coords are eclipsed at this phase angle, True otherwise'
-        return ((self.radius*np.sin(theta)*np.sin(phi) - self.star.distance*np.sin(alpha))**2 + (self.radius*np.cos(theta))**2 > self.star.radius**2)
-        
+        """returns False if these coords are eclipsed at this phase angle, True otherwise"""
+        return ((self.radius * np.sin(theta) * np.sin(phi) - self.star.distance * np.sin(alpha)) ** 2 + (
+                    self.radius * np.cos(theta)) ** 2 > self.star.radius ** 2)
+
     def light_curve(self, alpha):
-        'turns a phase curve into a light curve'
-        return self.radius**2 * self.star.luminosity * (1/(4*self.star.distance**2)) * self.phase_curve(alpha)
+        """turns a phase curve into a light curve"""
+        return self.radius ** 2 * self.star.luminosity * (1 / (4 * self.star.distance ** 2)) * self.phase_curve(alpha)
 
 
 class Ring:
     def __init__(self, albedo, inner_rad, outer_rad, normal, star):
         self.inner_radius = inner_rad
         self.outer_radius = outer_rad
-        self.sc_law = lambda mu_star:albedo/np.pi # isotropic scattering law intensity distribution - 1/pi factor from normalization
+        self.sc_law = lambda \
+            mu_star: albedo / np.pi  # isotropic scattering law intensity distribution - 1/pi factor from normalization
         self.normal = normal
         self.star = star
-        
+
     def get_mu_star(self, alpha):
-        'mu_star = cos(angle between star and normal to ring)'
+        """mu_star = cos(angle between star and normal to ring)"""
         star_pos = np.array([np.cos(alpha), np.sin(alpha), 0.])
         return np.dot(self.normal, star_pos)
-    
+
     def get_mu(self):
-        'mu = cos(angle between observer and normal to ring)'
+        """mu = cos(angle between observer and normal to ring)"""
         obs_pos = np.array([1, 0, 0])
         return np.dot(self.normal, obs_pos)
-        
+
     def phase_curve(self, alpha):
-        'phase curve innit'
+        """phase curve innit"""
         mu = self.get_mu()
         mu_star = self.get_mu_star(alpha)
-        return mu * mu_star * self.sc_law(mu_star) * (mu_star > 0) # boolean prevents forwards scattering
-    
+        return mu * mu_star * self.sc_law(mu_star) * (mu_star > 0)  # boolean prevents forwards scattering
+
     def secondary_eclipse(self, alpha):
-        'finds the amount to subtract from the ring - since there is no integral'
-        #struggling
-    
+        """finds the amount to subtract from the ring - since there is no integral"""
+        # struggling
+
     def light_curve(self, alpha):
-        return (self.outer_radius**2-self.inner_radius**2)*self.phase_curve(alpha)*self.star.luminosity/(4*self.star.distance**2)
+        return (self.outer_radius ** 2 - self.inner_radius ** 2) * self.phase_curve(alpha) * self.star.luminosity / (
+                    4 * self.star.distance ** 2)
+
 
 class Star:
     def __init__(self, luminosity, radius, distance, mass):
@@ -146,24 +155,25 @@ class Star:
         self.distance = distance
         self.mass = mass
 
-#%%%
-#debugging stuff
+
+# %%%
+# debugging stuff
 star = Star(1000000, 10, 50, 10)
 planet = Planet(1, 1, star)
 
 ring_normal = np.array([1., 1., 0.0])
-ring_normal /= np.sum(ring_normal*ring_normal)
+ring_normal /= np.sum(ring_normal * ring_normal)
 
 ring = Ring(1, 2, 3, ring_normal, star)
 
-animation = exoring_functions.Animation(planet,star,ring)
+animation = exoring_functions.Animation(planet, star, ring)
 animation.generate_animation()
 
 plt.style.use('the_usual.mplstyle')
 plt.figure()
-plt.plot(animation.alphas, animation.planet_curve, label = 'Planet')
-plt.plot(animation.alphas, animation.ring_curve, label = 'Ring')
-plt.plot(animation.alphas, animation.planet_curve + animation.ring_curve, label = 'Ring + Planet')
+plt.plot(animation.alphas, animation.planet_curve, label='Planet')
+plt.plot(animation.alphas, animation.ring_curve, label='Ring')
+plt.plot(animation.alphas, animation.planet_curve + animation.ring_curve, label='Ring + Planet')
 
 plt.xlabel(r'Phase angle $\alpha$')
 plt.ylabel(r'Intensity (arbitrary)')
