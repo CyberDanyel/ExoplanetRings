finally fixed the sign error
I hope

exoring_functions.py
@@ -136,8 +136,8 @@ def overlap_area(r_circle, r_ellipse, mu, cos_phi, sin_phi, offset):
         else:
             return 0.
 
-    x_prime = (x * cos_phi - y * sin_phi)
-    y_prime = (y * cos_phi + x * sin_phi) / mu
+    x_prime = (x * cos_phi + y * sin_phi)
+    y_prime = (y * cos_phi - x * sin_phi) / mu
     # plt.scatter(x, y)
     # plt.scatter(x_prime, y_prime)
 
@@ -157,7 +157,7 @@ def overlap_area(r_circle, r_ellipse, mu, cos_phi, sin_phi, offset):
 
     ellipse_section_area = mu * np.abs(
         circle_section_integral(r_ellipse,
-                                bounds=[np.sign(x[0]) * np.abs(x_ellipse_prime[0]), np.sign(offset) * r_ellipse]))
+                                bounds=[x_ellipse_prime[0], np.sign(offset) * r_ellipse]))
     return ellipse_section_area + circle_section_area
 
 

exoring_objects.py
@@ -255,7 +255,7 @@ class Ring:
             cos_phi = 1
         else:
             sin_phi = -n_y / sin_theta
-            cos_phi = n_z / sin_theta
+            cos_phi = np.abs(n_z) / sin_theta # if the normal is pointing up or down we want to rotate s. t. it aligns vertically
           
         outer_area = exoring_functions.overlap_area(self.star.radius, self.outer_radius, mu, cos_phi, sin_phi, y_star)
         inner_area = exoring_functions.overlap_area(self.star.radius, self.inner_radius, mu, cos_phi, sin_phi, y_star)

wavelength_testing.py
@@ -19,14 +19,15 @@ star = exoring_objects.Star(L_SUN, R_SUN, 0.1*AU, 1.)
 
 material = materials.RingMaterial('dustkapscatmat.inp', 1081, 500)
 sc_ring = scattering.WavelengthDependentScattering(material, bandpass, star.planck_function)
-sc_planet = scattering.Jupiter(.1)
+#sc_ring = scattering.Rayleigh(1.)
+sc_planet = scattering.Jupiter(1.)
 
 
-ringed_planet = exoring_objects.RingedPlanet(sc_planet, R_JUP, sc_ring, 1.5*R_JUP, 3.*R_JUP, [1., 1., 0.1], star)
+ringed_planet = exoring_objects.RingedPlanet(sc_planet, R_JUP, sc_ring, 1.5*R_JUP, 3.*R_JUP, [1., 1., -.1], star)
 
 alphas = list(np.linspace(-np.pi, -.1, 2000)) + list(np.linspace(-.1, .1, 5000)) + list(np.linspace(.1, np.pi, 2000))
 
-light_curve = ringed_planet.ring.light_curve(alphas)/star.L_wav(bandpass)
+light_curve = ringed_planet.light_curve(alphas)/star.L_wav(bandpass)
 
 plt.style.use('the_usual')
 plt.plot(alphas, light_curve)